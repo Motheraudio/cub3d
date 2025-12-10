f9750ef6bb5282aeb2170eef80bb3d721fe32a62
Michal Choma <mchoma@c3r1p4.42vienna.com>
2025-12-10 21:58:12 +0100

    get slice

--- render_test/get_wall_orientation.c
+++ render_test/get_wall_orientation.c
@@ -6,7 +6,7 @@
 /*   By: mchoma <your@mail.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 20:50:26 by mchoma            #+#    #+#             */
-/*   Updated: 2025/12/10 21:00:09 by mchoma           ###   ########.fr       */
+/*   Updated: 2025/12/10 21:33:26 by mchoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@ unsigned int	get_orientation(unsigned int colour)
 
 	mask = 0xFFFF00;
 	colour = colour & mask;
+	return (colour);
+}
+
+unsigned int	slice(unsigned int colour)
+{
+	unsigned int	mask;
+
+	mask = 0xFFFF00;
+	colour = colour & !mask;
 	return (colour);
 }
